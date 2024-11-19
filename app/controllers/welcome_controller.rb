9dc43cf75410749e83d0998eb261fd331ff7f10d
zunda <zundan@gmail.com>
2024-11-19 21:24:15 +0000

    Cache the time

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,8 @@
 class WelcomeController < ApplicationController
+  def index
+    @cached_time = Rails.cache.fetch("cached_time", expires_in: 5.seconds) do
+      Time.now
+    end
+    @host = ENV.fetch('DYNO', `hostname`).strip
+  end
 end
