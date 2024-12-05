303f9608bd050e6c78f6ef98eafaa9c6d4837c65
zunda <zundan@gmail.com>
2024-12-05 23:41:36 +0000

    Keep cache until server stops

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @cached_time = Rails.cache.fetch("cached_time", expires_in: 1.minute) do
+    @cached_time = Rails.cache.fetch("cached_time") do
       Time.now
     end
     @host = ENV.fetch("DYNO", `hostname`).strip
