2f6afc74cc03248c16e852e1d80a6d918ada1793
zunda <zundan@gmail.com>
2025-09-30 00:32:24 +0000

    Better handle errors from aviation weather

--- app/jobs/lookup_metar_job.rb
+++ app/jobs/lookup_metar_job.rb
@@ -10,8 +10,14 @@ class LookupMetarJob < ApplicationJob
     unless station =~ ICAO_REGEXP
       raise "#{station.inspect} isn't an ICAO airport code"
     end
-    metar = Net::HTTP.get(URI("https://#{AVIATION_WEATHER_SERVER}/api/data/metar?ids=#{station}"))
+    x = Net::HTTP.get_response(URI("https://#{AVIATION_WEATHER_SERVER}/api/data/metar?ids=#{station}"))
     timestamp = Time.now.to_f * 1000
+    metar = case x.response.code
+    when "200"
+      x.body
+    else
+      "#{station}: #{x.response.code} #{x.response.message}"
+    end
     ActionCable.server.broadcast("report", {time_ms: timestamp, message: metar})
   end
 end
